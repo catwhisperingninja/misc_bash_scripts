73f394cfdce1dbcb0027b9a9cfc128bc79815d83
Laura Lopez <25212708+catwhisperingninja@users.noreply.github.com>
2024-06-11 14:21:55 -0700

    Update vmFreshInstall.sh
    Brave install no longer works

--- vmFreshInstall.sh
+++ vmFreshInstall.sh
@@ -10,12 +10,6 @@ sudo apt install -y multiverse
 # sudo apt install -y ubuntu-restricted-extras
 sudo apt install -y libfuse2
 
-# Install Brave Browser
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install -y brave-browser
-
 # Install Visual Studio Code
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
