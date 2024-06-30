be89d7c01846760546ff136cb91277d19a861428
catwhisperer <catwhisperer@catwhisperer.org>
2024-06-29 21:11:29 -0700

    removed multiverse added fastfetch and brave

--- vmFreshInstall.sh
+++ vmFreshInstall.sh
@@ -6,7 +6,9 @@ sudo apt update
 # Install curl
 sudo apt install -y curl
 sudo apt install -y net-tools
-sudo apt install -y multiverse
+
+# Install fastfetch
+sudo add-aptrepository ppa:zhangsongcui3371/fastfetch
 
 # Install Visual Studio Code
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
@@ -15,7 +17,12 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" 
 sudo apt update
 sudo apt install -y code
 
-# adding comment
+# Install Brave Browser
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install -y brave-browser
 
 
 
