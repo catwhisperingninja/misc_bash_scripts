3969a5ce26a3f3020895ba90febb91a9c623653a
catwhisperingninja <25212708+catwhisperingninja@users.noreply.github.com>
2024-07-07 08:44:40 -0700

    brave browser is never going to work they change keys all the time

--- vmFreshInstall.sh
+++ vmFreshInstall.sh
@@ -6,6 +6,7 @@ sudo apt update
 # Install curl
 sudo apt install -y curl
 sudo apt install -y net-tools
+sudo apt install -y htop
 
 # Install Visual Studio Code
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
@@ -14,15 +15,8 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" 
 sudo apt update
 sudo apt install -y code
 
-# Install Brave Browser
-
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install -y brave-browser
-
 # install fastfetch
 
-sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch 
-sudo apt install -y fastfetch
+#sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch 
+#sudo apt install -y fastfetch
 
