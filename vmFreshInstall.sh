0a16a6778b40a726f2d2ac0f1dc5395e7dc6cfba
catwhisperingninja <25212708+catwhisperingninja@users.noreply.github.com>
2024-07-10 04:22:55 -0700

    quick adds for prox

--- vmFreshInstall.sh
+++ vmFreshInstall.sh
@@ -9,6 +9,23 @@ sudo apt install -y net-tools
 sudo apt install -y htop
 sudo apt install -y bridge-utils
 
+# Download the Mullvad signing key
+sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
+# Add the Mullvad repository server to apt
+echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
+# Install the package
+sudo apt update
+sudo apt install -y mullvad-vpn
+
+#download Warp
+sudo apt-get install -y wget gpg
+wget -qO- https://releases.warp.dev/linux/keys/warp.asc | gpg --dearmor > warpdotdev.gpg
+sudo install -D -o root -g root -m 644 warpdotdev.gpg /etc/apt/keyrings/warpdotdev.gpg
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/warpdotdev.gpg] https://releases.warp.dev/linux/deb stable main" > /etc/apt/sources.list.d/warpdotdev.list'
+rm warpdotdev.gpg
+sudo apt update
+sudo apt install -y warp-terminal
+
 # Install Visual Studio Code
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
@@ -17,7 +34,6 @@ sudo apt update
 sudo apt install -y code
 
 # install fastfetch
-
 sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch 
 sudo apt install -y fastfetch
 
