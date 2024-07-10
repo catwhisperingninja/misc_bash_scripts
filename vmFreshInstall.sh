6c79a01fa635a75d2310c2a4f0c03622731a5549
catwhisperingninja <25212708+catwhisperingninja@users.noreply.github.com>
2024-07-10 06:09:45 -0700

    prep Docker repo for install

--- vmFreshInstall.sh
+++ vmFreshInstall.sh
@@ -8,23 +8,10 @@ sudo apt install -y curl
 sudo apt install -y net-tools
 sudo apt install -y htop
 sudo apt install -y bridge-utils
-
-# Download the Mullvad signing key
-sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
-# Add the Mullvad repository server to apt
-echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
-# Install the package
-sudo apt update
-sudo apt install -y mullvad-vpn
-
-#download Warp
-sudo apt-get install -y wget gpg
-wget -qO- https://releases.warp.dev/linux/keys/warp.asc | gpg --dearmor > warpdotdev.gpg
-sudo install -D -o root -g root -m 644 warpdotdev.gpg /etc/apt/keyrings/warpdotdev.gpg
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/warpdotdev.gpg] https://releases.warp.dev/linux/deb stable main" > /etc/apt/sources.list.d/warpdotdev.list'
-rm warpdotdev.gpg
-sudo apt update
-sudo apt install -y warp-terminal
+sudo apt install -y jq
+sudo apt install -y git
+sudo apt install -y openssh-server
+sudo apt install -y openssh-client
 
 # Install Visual Studio Code
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
@@ -37,3 +24,17 @@ sudo apt install -y code
 sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch 
 sudo apt install -y fastfetch
 
+#prepare for Docker
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the Docker repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
