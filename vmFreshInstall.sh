6c296ff997ad9eb9eb6bba415a62b930da02635f
catwhisperingninja <25212708+catwhisperingninja@users.noreply.github.com>
2024-07-12 07:07:43 -0700

    simplified script on main branch

--- vmFreshInstall.sh
+++ vmFreshInstall.sh
@@ -7,8 +7,6 @@ sudo apt update
 sudo apt install -y curl
 sudo apt install -y net-tools
 sudo apt install -y htop
-sudo apt install -y bridge-utils
-sudo apt install -y jq
 sudo apt install -y git
 sudo apt install -y openssh-server
 sudo apt install -y openssh-client
@@ -24,17 +22,3 @@ sudo apt install -y code
 sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch 
 sudo apt install -y fastfetch
 
-#prepare for Docker
-# Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the Docker repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
